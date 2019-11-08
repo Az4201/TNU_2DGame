a1ad96bb395587b81c455b7725a5e8feb1ae63c8
Az4201 <56706504+Az4201@users.noreply.github.com>
2019-11-08 11:59:40 +0800

    建立腳本定義欄位

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 定義欄位 Field
+    //欄位類型 欄位名稱(指定 值)結尾
+    [Header("速度"), Range(0f, 100f)]
+    public float speed = 3.5f;   // 浮點數
+    [Header("跳躍"), Range(100, 2000)]
+    public int jump = 300;    //整數
+    [Header("是否在地板上"), Tooltip("用來判定角色是否在地板上。")]
+    public bool isGround = false; //布林值 - true false 
+    [Header("角色名稱")]
+    public string _name = "Az";  //字串 需要雙引號
+}
